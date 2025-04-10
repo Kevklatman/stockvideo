91a30ae267fc4c556b3409fd49bb11a979acbcc4
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2025-04-10 10:12:36 -0600

    Update AWS credentials to use environment provider and remove .env.example

--- server/src/controllers/upload.controller.ts
+++ server/src/controllers/upload.controller.ts
@@ -3,6 +3,7 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import crypto from 'crypto';
 import dotenv from 'dotenv';
+import { fromEnv } from '@aws-sdk/credential-providers';
 
 dotenv.config();
 
@@ -14,10 +15,7 @@ const UPLOAD_URL_EXPIRATION = 3600; // 1 hour in seconds
 // S3 Client initialization
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-  }
+  credentials: fromEnv()
 });
 
 // Types
